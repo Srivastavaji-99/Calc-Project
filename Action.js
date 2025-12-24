88664503e119c87e6742cc632cc1fb7f83ccc443
Himanshu Shrivastava <himanshusri0000@gmail.com>
2025-12-24 11:09:22 +0530

    againUpdate.js

--- Action.js
+++ Action.js
@@ -1,26 +1,53 @@
-let input = document.getElementById('inputBox');
+let showResult = document.querySelector('#input-box');
 let buttons = document.querySelectorAll('button');
+let expression = '';
 
-let string = "";
-let arr = Array.from(buttons);
-arr.forEach(button => {
-    button.addEventListener('click', (e) =>{
-        if(e.target.innerHTML == '='){
-            string = eval(string);
-            input.value = string;
-        }
-        else if(e.target.innerHTML == 'AC'){
-            string = "";
-            input.value = string;
-        }
-        else if(e.target.innerHTML == 'DEL'){
-            string = string.substring(0, string.length -1);
-            input.value = string;
-        }
-        else{
-           string += e.target.innerHTML;
-           input.value = string;
-        }
-        
-    })
-})
+buttons.forEach(button => {
+  button.addEventListener('click', () =>{
+    let value = button.innerText;
+    if(value === '÷'){
+      value = '/';
+    }
+    
+    if(value === 'X'){
+      expression = expression.slice(0, -1);
+      showResult.value = expression;
+    }else if(value === 'C'){
+      expression = "";
+      showResult.value = expression;
+    }else if(value === '='){
+      expression = eval(expression);
+      showResult.value = expression;
+    }else if(value === 'CE'){
+      clearEntery();
+    }else if(value === 'x²'){
+      expression = Math.pow(expression, 2);
+      showResult.value = expression;
+    }else if(value === '√x'){
+      expression = Math.sqrt(expression);
+      showResult.value = expression;
+    }else if(value === '1/x'){
+      expression = 1/expression;
+      showResult.value = expression;
+    }else if(value === '±'){
+      if(expression){
+        expression = (Number(expression) * -1).toString();
+        showResult.value = expression;
+      }
+    } else{
+      expression += value;
+      showResult.value = expression;
+    }
+  });
+});
+
+function clearEntery(){
+  while(expression.length > 0){
+    let lastChar = expression[expression.length -1];
+    if(lastChar === '+' || lastChar === '-' || lastChar === '*' || lastChar === '/' || lastChar === '%'){
+      break;
+    }
+    expression = expression.slice(0, -1);
+  }
+  showResult.value = expression;
+}
